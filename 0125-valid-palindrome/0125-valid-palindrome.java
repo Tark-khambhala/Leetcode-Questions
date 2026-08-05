be42f8565e6c16a7e4c318b1f153a572b7b75f8f
Tarkesh Khambhala <tarkeshkhambhala00@gmail.com>
2026-08-05 13:01:55 +0530

    Time: 14 ms (29.33%), Space: 46.3 MB (30.02%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean isPalindrome(String s) {
         s = s.toLowerCase();
         s=s.replaceAll("[^a-z0-9]","");
-        System.out.print(s);
         for (int i = 0; i < s.length()/2; i++) {
             if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
                 return false;
