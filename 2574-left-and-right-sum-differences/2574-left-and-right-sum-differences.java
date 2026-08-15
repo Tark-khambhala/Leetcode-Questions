78914334c3ab34ea240d98872029de07b084145f
Tarkesh Khambhala <tarkeshkhambhala00@gmail.com>
2026-08-15 11:47:40 +0530

    Time: 10 ms (9.11%), Space: 46.8 MB (15.38%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
@@ -4,14 +4,16 @@ class Solution {
         int x = 0;
         for (int i = 0; i < nums.length; i++) {
             int leftsum = 0, rightsum = 0;
+            if (i < nums.length - 1) {
                 for (int j = i + 1; j < nums.length; j++) {
                     rightsum += nums[j];
                 }
-            
+            }
+            if (i > 0) {
                 for (int j = i - 1; j >= 0; j--) {
                     leftsum += nums[j];
                 }
-            
+            }
             int diff = leftsum - rightsum;
             if (diff < 0) {
                 diff *= -1;
